32831f8aeaa6e2f9b694af620bb70002323a91d1
Adriano Galindo <adrianogalindo.git@gmail.com>
2019-04-03 21:06:24 -0400

    Creation TP InstitutGrasset

---
+++ app/src/main/java/com/example/adrianogalindotp3/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.adrianogalindotp3;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static int SPLASH_TIME_OUT = 6000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+
+        }, SPLASH_TIME_OUT);
+
+        VideoView mVideoView = findViewById(R.id.splash_VideoView);
+        String uriPath = "android.resource://com.example.adrianogalindotp3/" + R.raw.compositionfinale;
+
+        Uri uri = Uri.parse(uriPath);
+        mVideoView.setVideoURI(uri);
+        mVideoView.requestFocus();
+        mVideoView.start();
+
+
+    }
+}
